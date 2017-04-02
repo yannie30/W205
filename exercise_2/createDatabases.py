37cdc9669730725361b278c661fb4d92475062c1
w205 <w205@ip-172-31-40-149.ec2.internal>
2017-04-02 20:08:49 +0000

    exercise completed

---
+++ exercise_2/createDatabases.py
@@ -0,0 +1,28 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+# Connect to the database
+conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
+
+#Create the Database
+try:
+    # CREATE DATABASE can't run inside a transaction
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute("CREATE DATABASE tcount")
+    cur.close()
+    conn.close()
+except:
+    print "Could not create tcount"
+
+#Connecting to tcount
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+
+#Create a Table
+#The first step is to create a cursor. 
+
+cur = conn.cursor()
+cur.execute('''CREATE TABLE tweetwordcount
+       (word TEXT PRIMARY KEY     NOT NULL,
+       count INT     NOT NULL);''')
+conn.commit()
--- exercise_2/extweetwordcount/src/bolts/wordcount.py
+++ exercise_2/extweetwordcount/src/bolts/wordcount.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import Counter
 from streamparse.bolt import Bolt
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
 
@@ -9,18 +11,32 @@ class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
+	#create a connection for this instance
+	self.conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 
     def process(self, tup):
         word = tup.values[0]
-
+	
         # Write codes to increment the word count in Postgres
         # Use psycopg to interact with Postgres
         # Database name: Tcount 
         # Table name: Tweetwordcount 
         # you need to create both the database and the table in advance.
+        cur = self.conn.cursor()
+	
+        record_count=0
+        cur.execute("SELECT word, count FROM tweetwordcount WHERE word=%s", (word,))
+	records = cur.fetchall()
+	for rec in records:
+	    record_count = rec[1]
         
-
-        # Increment the local count
+	if record_count == 0:
+	    cur.execute("INSERT INTO tweetwordcount (word, count) VALUES (%s, %s)", (word,1))
+            self.conn.commit()
+        else:
+            cur.execute("UPDATE tweetwordcount SET count=%s WHERE word=%s", (record_count + 1, word))
+            self.conn.commit()
+	    
         self.counts[word] += 1
         self.emit([word, self.counts[word]])
 
